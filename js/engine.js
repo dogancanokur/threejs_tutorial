8d55dc656a5420028f8a336ed9485896d602b4b4
dogan.okur <dogan.okur@hititcs.com>
2026-03-11 17:52:57 +0300

    feat: refactor scene setup and add helper functions for grid and GUI integration

--- js/engine.js
+++ js/engine.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { GUI } from "dat.gui";
+import { gridHelper, guiInit } from "./helper";
 import { createBoxGeometry } from "./createGeometry";
 
 // --------------------------------------------------------------
@@ -11,7 +11,7 @@ function createScene() {
   window.scene = scene = new THREE.Scene();
 
   // Kamera oluştururken, görüş açısı, aspect ratio ve yakın/uzak kesme düzlemlerini belirtiyoruz
-  camera = new THREE.PerspectiveCamera(
+  window.camera = camera = new THREE.PerspectiveCamera(
     75, // fov
     window.innerWidth / window.innerHeight, // aspect ratio
     0.1, // near
@@ -19,23 +19,23 @@ function createScene() {
   );
 
   // sahne ve kamerayı birbirine bağlamak için renderleyici oluşturuyoruz
-  renderer = new THREE.WebGLRenderer();
+  window.renderer = renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
+
   // renderleyicinin oluşturduğu canvas elementini HTML sayfasına ekliyoruz
   document.getElementById("webgl-container").appendChild(renderer.domElement);
 
   // camera.position.z = 5; // kamerayı z ekseninde 5 birim geri çekiyoruz
-  camera.position.set(2,2,2); // kamerayı x, y, z eksenlerinde istediğimiz konuma ayarlayabiliriz
+  camera.position.set(2, 2, 2); // kamerayı x, y, z eksenlerinde istediğimiz konuma ayarlayabiliriz
 
   const gui = guiInit();
 
   // sahneye bir kutu ekleyelim ve sahneye ekleyelim
-  // scene.add(createBoxGeometry("green box", 1, 1, 2, 0x00ff00, gui));
-  // scene.add(createBoxGeometry("red box", 0.1, 3, 0.2, 0xff0000));
-  
+  scene.add(createBoxGeometry("green box", 1, 1, 2, 0x00ff00, gui));
+  scene.add(createBoxGeometry("red box", 0.1, 3, 0.2, 0xff0000));
+
   // ** bir posizyon vermezsek, varsayılan olarak (0, 0, 0) noktasında oluşturulur
 
-  
   var cameraLookAtPosition = new THREE.Vector3(0, 0, 0);
   // istersek doğrudan objenin lokasyonuna da çevirebiliriz
   // cameraLookAtPosition.copy(boxMesh.position);
@@ -69,22 +69,6 @@ function render() {
   requestAnimationFrame(render);
 }
 
-function guiInit() {
-  // GUI setup after adding the geometry
-  const gui = new GUI();
-
-  cameraConfig(gui);
-
-  function cameraConfig(gui) {
-    const cameraFolder = gui.addFolder("Camera Position");
-    THREE.warn("Camera position controls added to GUI." + camera.position.toArray());
-    cameraFolder.add(camera.position, "x", -5, 5);
-    cameraFolder.add(camera.position, "y", -5, 5);
-    cameraFolder.add(camera.position, "z", -5, 5);
-  }
-  return gui;
-}
-
 /**
  * pencere boyutu değiştiğinde kameranın aspect ratio'sunu güncelle ve renderleyicinin boyutunu yeniden ayarla
  */
@@ -94,25 +78,3 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 window.addEventListener("resize", onWindowResize, false);
-
-/**
- * sahneye bir grid helper ekleyelim, bu bize sahnenin zeminini görsel olarak göstermek için yardımcı olur
- */
-function gridHelper() {
-  // grid helper, sahnenin zeminini görsel olarak göstermek için yardımcı olur
-  const gridHelper = new THREE.GridHelper(5, 10);
-  scene.add(gridHelper);
-
-  // xyz eksenlerini göstermek için bir axes helper ekleyelim
-  const axesHelper = new THREE.AxesHelper(10);
-  scene.add(axesHelper);
-
-  const arrowHelper = new THREE.ArrowHelper(
-    // yön vektörünü normalize ediyoruz
-    new THREE.Vector3(1, 1, 1).normalize(), // yön vektörü (yukarı)
-    new THREE.Vector3(0, 0, 0), // başlangıç noktası
-    2, // uzunluk
-    0xffff00, // renk (sarı)
-  );
-  scene.add(arrowHelper);
-}
---
+++ js/helper.js
@@ -0,0 +1,56 @@
+import { GUI } from "dat.gui";
+import * as THREE from "three";
+
+// --------------------------------------------------------------
+
+/**
+ * sahneye bir grid helper ekleyelim, bu bize sahnenin zeminini görsel olarak göstermek için yardımcı olur
+ */
+export function gridHelper() {
+  // grid helper, sahnenin zeminini görsel olarak göstermek için yardımcı olur
+  const gridHelper = new THREE.GridHelper(5, 10);
+  scene.add(gridHelper);
+
+  // xyz eksenlerini göstermek için bir axes helper ekleyelim
+  const axesHelper = new THREE.AxesHelper(3);
+  //   scene.add(axesHelper);
+
+  const arrowHelper = new THREE.ArrowHelper(
+    // yön vektörünü normalize ediyoruz
+    new THREE.Vector3(1, 1, 1).normalize(), // yön vektörü (yukarı)
+    new THREE.Vector3(0, 0, 0), // başlangıç noktası
+    2, // uzunluk
+    0xffff00, // renk (sarı)
+  );
+  //   scene.add(arrowHelper);
+
+  const secondCamera = new THREE.PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    10,
+    100,
+  );
+  secondCamera.position.x = -5;
+  secondCamera.position.y = -5;
+  secondCamera.position.z = -5;
+  const cameraHelper = new THREE.CameraHelper(secondCamera);
+//   scene.add(cameraHelper);
+}
+
+export function guiInit() {
+  // GUI setup after adding the geometry
+  const gui = new GUI();
+
+  cameraConfig(gui);
+
+  function cameraConfig(gui) {
+    const cameraFolder = gui.addFolder("Camera Position");
+    THREE.warn(
+      "Camera position controls added to GUI." + camera.position.toArray(),
+    );
+    cameraFolder.add(camera.position, "x", -5, 5);
+    cameraFolder.add(camera.position, "y", -5, 5);
+    cameraFolder.add(camera.position, "z", -5, 5);
+  }
+  return gui;
+}
