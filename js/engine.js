e1cc161817600a66cc7695cbcae181675eca250f
dogan.okur <dogan.okur@hititcs.com>
2026-03-24 11:52:58 +0300

    feat: update scene setup to include GLTF model loading and enhance spotlight properties

--- js/engine.js
+++ js/engine.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { gridHelper, guiInit as datGuiInit } from "./helper";
 import createBoxGeometry from "./createGeometry";
+import { createGLTFModel } from "./modelLoader";
 
 // --------------------------------------------------------------
 
@@ -53,7 +53,7 @@ function createScene() {
     ),
   );
   scene.add(
-    createBoxGeometry("floor", 5, 1, 5, 0xf0000a, 0, -0.5, 0, true, true),
+    createBoxGeometry("floor", 7, 1, 7, 0xf0000a, 0, -0.5, 0, true, true),
   );
 
   // ** bir posizyon vermezsek, varsayılan olarak (0, 0, 0) noktasında oluşturulur
@@ -65,11 +65,11 @@ function createScene() {
 
   gridHelper();
 
-  //   createGLTFModel(
-  // "models/airbus/scene.gltf",
-  // "models/airbus/textures/material02_baseColor.jpg",
-  // "airbus1",
-  //   );
+  // createGLTFModel(
+  //   "models/airbus/scene.gltf",
+  //   "models/airbus/textures/material02_baseColor.jpg",
+  //   "airbus1",
+  // );
   render();
 }
 
@@ -78,23 +78,58 @@ window.onload = createScene;
 
 function spawnSpotLight() {
   const spotLight = new THREE.SpotLight(0xffffff, 10);
-  spotLight.position.set(2, 3, 3);
+  spotLight.position.set(2, 3, 2);
   spotLight.castShadow = true;
-  spotLight.shadow.mapSize.width = 2048;
-  spotLight.shadow.mapSize.height = 2048;
-  spotLight.name = "spotLight";
+  spotLight.decay = 2;
+  spotLight.shadow.mapSize.width = 4096;
+  spotLight.shadow.mapSize.height = 4096;
+  spotLight.name = "spotLight1";
+
+  // spotLight.shadow.camera.near = 0.1;
+
+  var target = new THREE.Object3D();
+  target.position.set(0, 0, 0);
+  target.name = "spotLightTarget1";
+  scene.add(target);
+  spotLight.target = target;
+
   scene.add(spotLight);
 
   let spotLightGui = datGui.addFolder("Spot Light Properties");
-  spotLightGui.add(scene.getObjectByName("spotLight"), "penumbra", 0, 1); // penumbra, spot ışığının kenarlarının ne kadar yumuşak olduğunu kontrol eder, 0 tam keskin kenarlar, 1 ise tamamen yumuşak ken
-  spotLightGui.add(scene.getObjectByName("spotLight"), "intensity", 0, 100); // intensity, ışığın parlaklığını kontrol eder, 0 hiç ışık yok, 100 çok parlak ışık
-  spotLightGui.add(scene.getObjectByName("spotLight"), "distance", 0, 10); // distance, ışığın etkili olduğu maksimum mesafeyi kontrol eder, 0 ise sonsuz mesafe, 10 ise 10 birim uzaklığa kadar etkili olur
-  spotLightGui.add(scene.getObjectByName("spotLight"), "decay", 0, 100); // decay, ışığın mesafeye göre ne kadar hızlı azaldığını kontrol eder, 0 ise hiç azalma olmaz, 1 ise gerçek dünyadaki gibi azalma olur, 2 ise daha hızlı azalma olur
-  spotLightGui.add(scene.getObjectByName("spotLight"), "power", 0, 100); // power, ışığın toplam gücünü kontrol eder, 0 hiç ışık yok, 100 çok güçlü ışık
+  spotLightGui.add(scene.getObjectByName("spotLight1"), "penumbra", 0, 2); // penumbra, spot ışığının kenarlarının ne kadar yumuşak olduğunu kontrol eder, 0 tam keskin kenarlar, 1 ise tamamen yumuşak ken
+  spotLightGui.add(scene.getObjectByName("spotLight1"), "intensity", 0, 100); // intensity, ışığın parlaklığını kontrol eder, 0 hiç ışık yok, 100 çok parlak ışık
+  spotLightGui.add(scene.getObjectByName("spotLight1"), "distance", 0, 10); // distance, ışığın etkili olduğu maksimum mesafeyi kontrol eder, 0 ise sonsuz mesafe, 10 ise 10 birim uzaklığa kadar etkili olur
+  spotLightGui.add(scene.getObjectByName("spotLight1"), "decay", 0, 5); // decay, ışığın mesafeye göre ne kadar hızlı azaldığını kontrol eder, 0 ise hiç azalma olmaz, 1 ise gerçek dünyadaki gibi azalma olur, 2 ise daha hızlı azalma olur
+  spotLightGui.add(scene.getObjectByName("spotLight1"), "power", 0, 100); // power, ışığın toplam gücünü kontrol eder, 0 hiç ışık yok, 100 çok güçlü ışık
 
-  spotLightGui.add(scene.getObjectByName("spotLight").position, "x", -10, 10);
-  spotLightGui.add(scene.getObjectByName("spotLight").position, "y", -10, 10);
-  spotLightGui.add(scene.getObjectByName("spotLight").position, "z", -10, 10);
+  spotLightGui.add(scene.getObjectByName("spotLight1").position, "x", -10, 10);
+  spotLightGui.add(scene.getObjectByName("spotLight1").position, "y", -10, 10);
+  spotLightGui.add(scene.getObjectByName("spotLight1").position, "z", -10, 10);
+
+  let spotLightTargetGui = spotLightGui.addFolder("Spot Light Target Position");
+  spotLightTargetGui.add(
+    scene.getObjectByName("spotLightTarget1").position,
+    "x",
+    -10,
+    10,
+  );
+  spotLightTargetGui.add(
+    scene.getObjectByName("spotLightTarget1").position,
+    "y",
+    -10,
+    10,
+  );
+  spotLightTargetGui.add(
+    scene.getObjectByName("spotLightTarget1").position,
+    "z",
+    -10,
+    10,
+  );
+
+  var helper = new THREE.SpotLightHelper(spotLight);
+  scene.add(helper);
+  var cameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+  scene.add(cameraHelper);
 }
 
 /**
@@ -110,9 +145,9 @@ function render() {
   //     scene.getObjectByName("green box").rotation.y += 0.005;
   //   }
 
-  if (scene.getObjectByName("airbus1")) {
-    scene.getObjectByName("airbus1").rotation.y += 0.005;
-  }
+  // if (scene.getObjectByName("airbus1")) {
+  //   scene.getObjectByName("airbus1").rotation.y += 0.005;
+  // }
 
   // render fonksiyonunu sürekli çağırmak için requestAnimationFrame kullanıyoruz
   requestAnimationFrame(render);
--- js/modelLoader.js
+++ js/modelLoader.js
@@ -1,20 +1,31 @@
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/Addons.js";
+
+// --------------------------------------------------------------
+
 const textureLoader = new THREE.TextureLoader();
 const gltfLoader = new GLTFLoader();
-gltfLoader.load("models/airbus/scene.gltf", function (gltf) {
-  gltf.scene.scale.set(0.2, 0.2, 0.2);
-  const texture = textureLoader.load(
-    "models/airbus/textures/material02_baseColor.jpg",
-  );
-  const material = new THREE.MeshBasicMaterial({ map: texture });
-  gltf.scene.material = material;
-  scene.add(gltf.scene);
-});
+
+function createExpamleModel() {
+  gltfLoader.load("models/airbus/scene.gltf", function (gltf) {
+    gltf.scene.scale.set(0.2, 0.2, 0.2);
+    const texture = textureLoader.load(
+      "models/airbus/textures/material02_baseColor.jpg",
+    );
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    gltf.scene.material = material;
+    scene.add(gltf.scene);
+  });
+}
 
 export function createGLTFModel(gltfPath, texturePath, meshName) {
   const textureLoader = new THREE.TextureLoader();
   const gltfLoader = new GLTFLoader();
   gltfLoader.load(gltfPath, function (gltf) {
     gltf.scene.scale.set(0.1, 0.1, 0.1);
+    gltf.scene.position.set(0, 0.5, 0);
+    gltf.scene.castShadow = true;
+    gltf.scene.receiveShadow = true;
     const texture = textureLoader.load(texturePath);
     const material = new THREE.MeshStandardMaterial({ map: texture });
     gltf.scene.material = material;
